55917d6f1a8c1a7c93239e82efa1465b044e43b2
Charlie Harding <charlie_harding@icloud.com>
2022-04-23 03:15:54 +0100

    Remove unused vue-head

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,12 +1,6 @@
 import { createHead } from '@vueuse/head';
 import { createApp } from 'vue';
-import VueHead from 'vue-head';
 
 import App from '@/App.vue';
 
-createApp(App)
-  // VueHead is used for Options components
-  .use(VueHead)
-  // createHead is used for Composition components
-  .use(createHead())
-  .mount('body');
+createApp(App).use(createHead()).mount('body');
