5c78738cbf9cf78b8dbc2f64470dd5916e4ddbf8
Charlie Harding <charlie_harding@icloud.com>
2022-04-23 18:40:53 +0100

    Replace $emit with emit

--- client/src/components/ActivityItem.vue
+++ client/src/components/ActivityItem.vue
@@ -1,9 +1,9 @@
 <template>
   <li
     :class="['activity-item', { selected }]"
-    @click="$emit('click', $event)"
-    @touchstart="$emit('touchstart')"
-    @dblclick="$emit('dblclick', $event)"
+    @click="emit('click', $event)"
+    @touchstart="emit('touchstart')"
+    @dblclick="emit('dblclick', $event)"
   >
     <div class="activity-name" v-text="activity.name" />
     <div v-if="!activity.map" class="spinner">
