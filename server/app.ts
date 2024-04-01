668d584a478bc7a04262f44585627df797704c92
Charlie Harding <charlie_harding@icloud.com>
2024-04-01 11:35:59 +0200

    Fix history API

--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,8 @@
-// @ts-check
-
 import { SERVER_DOMAIN, SERVER_PORT, VITE_APP_NAME } from '@strava-heatmapper/shared/config/dotenv';
 import bodyParser from 'body-parser';
 import history from 'connect-history-api-fallback';
 import cookieParser from 'cookie-parser';
-import express from 'express';
+import express, { type RequestHandler } from 'express';
 import expressWs from 'express-ws';
 
 import apiRouter from './api';
@@ -15,18 +13,24 @@ app.use(cookieParser());
 
 app.use(bodyParser.json());
 
-function corsConfig(req, res, next) {
+const corsConfig: RequestHandler = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', SERVER_DOMAIN);
-  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
-}
+};
 
 app.use(corsConfig);
 
 app.use('/api', apiRouter(SERVER_DOMAIN));
 
+// Support history api
+app.use(
+  history({
+    index: '',
+  }),
+);
 const compiledFrontEnd = __filename.includes('/dist/') ? '../client' : '../dist/client';
 
 // Middleware for serving frontend directory
@@ -34,13 +38,6 @@ const staticFileMiddleware = express.static(compiledFrontEnd);
 
 // 1st call for unredirected requests
 app.use(staticFileMiddleware);
-
-// Support history api
-app.use(
-  history({
-    index: `${compiledFrontEnd}/index.html`,
-  }),
-);
 
 app.listen(SERVER_PORT, () => {
   console.log(`${VITE_APP_NAME} backend listening on port ${SERVER_PORT}.`);
