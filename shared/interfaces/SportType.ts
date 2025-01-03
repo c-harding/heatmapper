08c6b3c1cdd419f5cf6f6e96f7d979c3f1152234
Charlie Harding <charlie_harding@icloud.com>
2025-01-03 00:58:10 +0000

    Fix “all biking” group

--- shared/interfaces/SportType.ts
+++ shared/interfaces/SportType.ts
@@ -57,7 +57,7 @@ export const sportGroups: Record<string, string> = {
   'Hike,Snowshoe,Run,TrailRun,Walk': 'All on foot',
   'Run,TrailRun': 'All running',
   'Hike,Walk': 'All walking',
-  'EBikeRide,GravelRide,Handcycle,MountainBikeRide,Velomobile,VirtualRide': 'All biking',
+  'EBikeRide,GravelRide,Handcycle,MountainBikeRide,Ride,Velomobile,VirtualRide': 'All biking',
   'AlpineSki,BackcountrySki,IceSkate,Snowboard,Snowshoe,NordicSki': 'All winter sports',
 };
 
