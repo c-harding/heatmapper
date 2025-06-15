c38e1bb3d0fdf0ef3a5315ef676bf54c2a967fb7
Charlie Harding <charlie_harding@icloud.com>
2025-06-15 15:43:01 +0200

    Add button to save filters

---
+++ client/src/types/FilterModel.ts
@@ -0,0 +1,15 @@
+export interface RangeFilter {
+  // These fields are both readonly to avoid problems with shallow copies of the filter model
+  readonly min?: number;
+  readonly max?: number;
+}
+
+export interface FilterModel {
+  sportType?: string;
+
+  /** Set to true to only show starred routes, or false to only show unfiltered routes */
+  starred?: boolean;
+
+  distance?: RangeFilter;
+  elevation?: RangeFilter;
+}
