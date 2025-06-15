ec5dd81581fa689a8226791bbd21ebda772fc007
Charlie Harding <charlie_harding@icloud.com>
2025-06-15 15:14:38 +0200

    Add filters and filter reset button

--- client/src/components/sidebar/SidebarFilter.vue
+++ client/src/components/sidebar/SidebarFilter.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 
-import { useActivityStore } from '@/stores/ActivityStore';
+import { type FilterModel, type RangeFilter, useActivityStore } from '@/stores/ActivityStore';
 import { useSportTypeStore } from '@/stores/SportTypeStore';
+import { formatKilometers, formatMeters } from '@/utils/numberFormat';
 
 import UIVerticalTab from '../ui/tabs/UIVerticalTab.vue';
 import UIVerticalTabContainer from '../ui/tabs/UIVerticalTabContainer.vue';
 import UIButton from '../ui/UIButton.vue';
+import UIButtonGroup from '../ui/UIButtonGroup.vue';
 import UIDropdown from '../ui/UIDropdown.vue';
+import UIMultiText from '../ui/UIMultiText.vue';
+import UIRange from '../ui/UIRange.vue';
 import controlsStyle from './controls.module.scss';
 
 const activityStore = useActivityStore();
@@ -25,16 +29,63 @@ const chosenSportLabel = computed(() => {
   return undefined;
 });
 
-const filterSummary = computed(
-  () =>
-    [
-      chosenSportLabel.value,
-      activityStore.useRoutes &&
-        activityStore.filterModel.starred !== undefined &&
-        (activityStore.filterModel.starred ? 'only starred' : 'only unstarred'),
-    ]
-      .filter((string): string is string => !!string)
-      .join(', ') || 'No filter',
+const blankFilter = Object.freeze<FilterModel>({
+  distance: undefined,
+  elevation: undefined,
+  sportType: undefined,
+  starred: undefined,
+});
+
+function compareRanges(rangeA: RangeFilter | undefined, rangeB: RangeFilter | undefined) {
+  return rangeA?.max === rangeB?.max && rangeA?.min === rangeB?.min;
+}
+
+function compareFilters(filterA: FilterModel, filterB: FilterModel) {
+  return (
+    (filterA.sportType ?? '') === (filterB.sportType ?? '') &&
+    filterA.starred === filterB.starred &&
+    compareRanges(filterA.distance, filterB.distance) &&
+    compareRanges(filterA.elevation, filterB.elevation)
+  );
+}
+
+const previousFilters = ref<FilterModel>();
+
+const filterState = computed<'changed' | 'canUndo' | 'initial'>(() => {
+  if (!compareFilters(activityStore.filterModel, blankFilter)) return 'changed';
+  else if (previousFilters.value) return 'canUndo';
+  else return 'initial';
+});
+
+function revertFilter() {
+  if (!compareFilters(activityStore.filterModel, blankFilter)) {
+    previousFilters.value = { ...activityStore.filterModel };
+    Object.assign(activityStore.filterModel, blankFilter);
+  } else {
+    Object.assign(activityStore.filterModel, previousFilters.value);
+  }
+}
+
+function formatRange(
+  range: RangeFilter | undefined,
+  label: string,
+  format: (value: number) => string,
+): string | undefined {
+  if (range?.min && range?.max) return `${format(range.min)} ≤ ${label} ≤ ${format(range.max)}`;
+  if (range?.min) return `${label} ≥ ${format(range.min)}`;
+  if (range?.max) return `${label} ≤ ${format(range.max)}`;
+  return undefined;
+}
+
+const filterSummary = computed(() =>
+  [
+    chosenSportLabel.value,
+    formatRange(activityStore.filterModel.distance, 'distance', formatKilometers),
+    formatRange(activityStore.filterModel.elevation, 'elevation', formatMeters),
+    activityStore.useRoutes &&
+      activityStore.filterModel.starred !== undefined &&
+      (activityStore.filterModel.starred ? 'only starred' : 'only unstarred'),
+  ].filter((string): string is string => !!string),
 );
 </script>
 
@@ -43,12 +94,16 @@ const filterSummary = computed(
     <UIVerticalTab :tab="makeTab('filter')" icon="filter_alt" :contentClass="$style.content">
       <template #summary="{ select }">
         <div :class="$style.summary" @click="select">
-          {{ filterSummary }}
+          <template v-for="(summaryItem, i) of filterSummary" :key="i"
+            ><span>{{ summaryItem }}</span
+            ><template v-if="i + 1 < filterSummary.length">, </template></template
+          >
+          <template v-if="!filterSummary.length">No filter set</template>
         </div>
       </template>
       <template #expanded>
         <div :class="controlsStyle.grid">
-          <label :class="controlsStyle.expand">
+          <label>
             <span>Sport type</span>
             <UIDropdown
               v-model="sportTypeStore.filter"
@@ -57,6 +112,17 @@ const filterSummary = computed(
               blankLabel="All sports"
             />
           </label>
+
+          <label>
+            <span>Distance</span>
+            <UIRange v-model="activityStore.filterModel.distance" :step="1" :scale="0.001" />
+          </label>
+
+          <label>
+            <span>Elevation</span>
+            <UIRange v-model="activityStore.filterModel.elevation" :step="0.1" />
+          </label>
+
           <label
             v-if="activityStore.useRoutes"
             title="Only show starred routes (double-click for unstarred routes)"
@@ -72,6 +138,20 @@ const filterSummary = computed(
               @dbl-click="activityStore.filterModel.starred = false"
             />
           </label>
+          <div :class="controlsStyle.buttons">
+            <UIButtonGroup>
+              <UIButton
+                :disabled="filterState === 'initial'"
+                :icon="filterState === 'canUndo' ? 'undo' : 'delete'"
+                @click="revertFilter"
+              >
+                <UIMultiText
+                  :texts="{ reset: 'Reset', undo: 'Undo' }"
+                  :selected="filterState === 'canUndo' ? 'undo' : 'reset'"
+                />
+              </UIButton>
+            </UIButtonGroup>
+          </div>
         </div>
       </template>
     </UIVerticalTab>
@@ -87,5 +167,9 @@ const filterSummary = computed(
 .summary {
   font-size: 0.9em;
   font-style: italic;
+
+  > span {
+    white-space: nowrap;
+  }
 }
 </style>
