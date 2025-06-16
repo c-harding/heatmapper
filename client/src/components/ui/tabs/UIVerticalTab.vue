a5c9335d5f421a1db0c4949bb92461f0d99dfbb1
Charlie Harding <charlie_harding@icloud.com>
2025-06-16 22:14:09 +0200

    Add heading for filter group

--- client/src/components/ui/tabs/UIVerticalTab.vue
+++ client/src/components/ui/tabs/UIVerticalTab.vue
@@ -6,7 +6,8 @@ import { type Tab } from './UIVerticalTabContainer.vue';
 defineProps<{
   icon?: string;
   tab: Tab<T>;
-  contentClass?: unknown;
+  expandedContentClass?: unknown;
+  heading?: string;
   summary?: string;
 }>();
 </script>
@@ -14,17 +15,28 @@ defineProps<{
 <template>
   <div :class="[style.uiVerticalTab, tab.isOpen && style.selected]">
     <div :class="style.tab">
-      <button :class="style.button" @click="tab.toggle">
-        <slot name="icon"><UIIcon :icon inline /></slot>
+      <button :class="style.button" :title="heading" @click="tab.toggle">
+        <div :class="style.buttonContent">
+          <slot name="icon"><UIIcon :icon inline /></slot>
+        </div>
       </button>
     </div>
 
-    <div :class="[style.content, contentClass]">
+    <div :class="[style.content]">
       <slot :isOpen="tab.isOpen" :toggle="tab.toggle" :select="tab.select">
-        <slot v-if="tab.isOpen" name="expanded" :toggle="tab.toggle" />
+        <slot v-if="tab.isOpen" name="expanded" :toggle="tab.toggle">
+          <h2 v-if="heading" :class="style.heading">{{ heading }}</h2>
+          <div :class="expandedContentClass">
+            <slot v-if="tab.isOpen" name="expandedContent" :toggle="tab.toggle" />
+          </div>
+        </slot>
         <slot v-else name="summary" :select="tab.select">
-          <div :class="style.summary" @click="tab.select()">
-            <slot name="styledSummary" :select="tab.select">{{ summary }}</slot>
+          <div :class="style.summaryRow" @click="tab.select()">
+            <span v-if="heading" :class="style.heading">{{ heading }}</span
+            >{{ ' ' }}
+            <span :class="style.summary">
+              <slot name="styledSummary" :select="tab.select">{{ summary }}</slot>
+            </span>
           </div>
         </slot>
       </slot>
