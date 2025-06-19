2a7f3a7c26c03fc338ba4e3a710cf7919ae094eb
Charlie Harding <charlie_harding@icloud.com>
2025-06-19 19:05:21 +0200

    Use details and simplify UI for header

--- client/src/components/ui/tabs/UIVerticalTab.vue
+++ client/src/components/ui/tabs/UIVerticalTab.vue
@@ -3,43 +3,48 @@ import UIIcon from '../UIIcon.vue';
 import style from './tab.module.scss';
 import { type Tab } from './UIVerticalTabContainer.vue';
 
-defineProps<{
+const { tab } = defineProps<{
   icon?: string;
   tab: Tab<T>;
   expandedContentClass?: unknown;
   heading?: string;
   summary?: string;
 }>();
+
+function preventIfSelected(event: MouseEvent) {
+  if (window.getSelection()?.type === 'Range') {
+    event.preventDefault();
+  }
+}
+
+function onToggle(event: ToggleEvent) {
+  tab.setOpen(event.newState === 'open');
+}
 </script>
 
 <template>
   <div :class="[style.uiVerticalTab, tab.isOpen && style.selected]">
     <div :class="style.tab">
-      <button :class="style.button" :title="heading" @click="tab.toggle">
+      <button :class="style.button" :title="heading" @click="tab.toggle()">
         <div :class="style.buttonContent">
           <slot name="icon"><UIIcon :icon inline /></slot>
         </div>
       </button>
     </div>
 
-    <div :class="[style.content]">
-      <slot :isOpen="tab.isOpen" :toggle="tab.toggle" :select="tab.select">
-        <slot v-if="tab.isOpen" name="expanded" :toggle="tab.toggle">
-          <h2 v-if="heading" :class="style.heading">{{ heading }}</h2>
-          <div :class="expandedContentClass">
-            <slot v-if="tab.isOpen" name="expandedContent" :toggle="tab.toggle" />
-          </div>
+    <details :class="[style.content]" :open="tab.isOpen" @toggle="onToggle">
+      <summary :class="style.summaryRow" @click="preventIfSelected">
+        <slot :isOpen="tab.isOpen" :toggle="tab.toggle" :select="tab.select">
+          <span v-if="heading" :class="style.heading">{{ heading }}</span>
+          {{ ' ' }}
+          <span v-if="!tab.isOpen" :class="style.summary">
+            <slot name="summary" :select="tab.select">{{ summary }}</slot>
+          </span>
         </slot>
-        <slot v-else name="summary" :select="tab.select">
-          <div :class="style.summaryRow" @click="tab.select()">
-            <span v-if="heading" :class="style.heading">{{ heading }}</span
-            >{{ ' ' }}
-            <span :class="style.summary">
-              <slot name="styledSummary" :select="tab.select">{{ summary }}</slot>
-            </span>
-          </div>
-        </slot>
-      </slot>
-    </div>
+      </summary>
+      <div :class="expandedContentClass">
+        <slot name="expanded" :toggle="tab.toggle" />
+      </div>
+    </details>
   </div>
 </template>
