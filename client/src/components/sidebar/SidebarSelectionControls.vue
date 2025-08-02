00fcf2683df5d41def0ddb53f8f598e82c1299d6
Charlie Harding <charlie_harding@icloud.com>
2025-08-02 17:09:23 +0200

    Repeat summary in expanded view for selection block

--- client/src/components/sidebar/SidebarSelectionControls.vue
+++ client/src/components/sidebar/SidebarSelectionControls.vue
@@ -10,11 +10,13 @@ import {
   countSelectedRoutes,
 } from '@/utils/strings';
 
+import tabStyle from '../ui/tabs/tab.module.scss';
 import UIVerticalTab from '../ui/tabs/UIVerticalTab.vue';
 import { type Tab } from '../ui/tabs/UIVerticalTabContainer.vue';
 import UIButton from '../ui/UIButton.vue';
 import UIButtonGroup from '../ui/UIButtonGroup.vue';
 import UICheckbox from '../ui/UICheckbox.vue';
+import UIUnbrokenList from '../ui/UIUnbrokenList.vue';
 import SelectionHelp from './SelectionHelp.vue';
 
 defineProps<{ tab: Tab<string> }>();
@@ -42,6 +44,8 @@ function reset() {
 }
 
 const showHelp = ref(false);
+
+const summaryEmpty = 'None selected';
 </script>
 
 <template>
@@ -50,7 +54,7 @@ const showHelp = ref(false);
     icon="done_all"
     heading="Selection"
     :summaryItems
-    summaryEmpty="None selected"
+    :summaryEmpty
     :resetDisabled="
       selectionStore.selectionMode === false &&
       !selectionStore.lockedSelection &&
@@ -60,6 +64,9 @@ const showHelp = ref(false);
     @help="showHelp = true"
   >
     <template #expanded>
+      <UIUnbrokenList :items="summaryItems" :class="tabStyle.summary">{{
+        summaryEmpty
+      }}</UIUnbrokenList>
       <UICheckbox v-model="selectionStore.selectionMode">Multiple selection mode</UICheckbox>
       <UIButtonGroup>
         <UIButton
