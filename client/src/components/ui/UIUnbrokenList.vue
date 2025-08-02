07b332455d1170a974da5ece26a1c9048b09de36
Charlie Harding <charlie_harding@icloud.com>
2025-08-02 17:09:23 +0200

    Repeat summary in expanded view for selection block

---
+++ client/src/components/ui/UIUnbrokenList.vue
@@ -0,0 +1,25 @@
+<!--
+  A simple component to render a list of items without breaking individual items onto new lines.
+
+  The default slot is used to provide a fallback when there are no items to show.
+-->
+<script setup lang="ts">
+const { items, empty } = defineProps<{ items: string[] | undefined; empty?: string }>();
+</script>
+
+<template>
+  <span
+    ><template v-if="items?.length">
+      <template v-for="(item, i) of items" :key="i"
+        ><span :class="$style.summarySpan">{{ item }}</span
+        ><template v-if="i + 1 < items.length">, </template></template
+      ></template
+    ><slot v-else>{{ empty }}</slot></span
+  >
+</template>
+
+<style lang="scss" module>
+.summarySpan {
+  white-space: nowrap;
+}
+</style>
